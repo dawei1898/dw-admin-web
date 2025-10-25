d42b5f0b6946dab480cb24a96d079ebd79283701
dawei <187533154+dawei1898@users.noreply.github.com>
2025-10-25 13:16:00 +0800

    feat：Breadcrumbs 面包屑

---
+++ src/components/Breadcrumbs.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {useMatches} from "react-router";
+import {Breadcrumb} from "antd";
+
+
+
+/**
+ * 根据路由生成面包屑
+ */
+const Breadcrumbs = () => {
+
+    const matches = useMatches();
+
+    // matches 是一个数组，每个 match 包含： route, params, pathname, etc.
+    const crumbs = matches
+        .filter((match) => {
+            // 只保留有 breadcrumb 定义的 route
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            return match.handle?.breadcrumb != null;
+        })
+        .map((match, index) => {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            const title: React.ReactNode  = match.handle!.breadcrumb;
+
+            // 最后一个 item 不需要链接
+            const isLast = index === matches.length - 1;
+
+            return {
+                key: match.pathname,
+                title: title,
+                href: isLast ? undefined :match.pathname,
+            };
+        });
+
+    return <Breadcrumb items={crumbs} />;
+};
+
+export default Breadcrumbs;
